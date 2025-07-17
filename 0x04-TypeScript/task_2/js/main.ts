a211c3374400c2d8e6df42ca52058e2fabe9cc1d
BlessmanJay <blessmanjay@gmail.com>
2025-07-17 06:26:57 +0100

    Add Task 7: String Literal Types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,4 +1,4 @@
-// Task #5
+// Task #5: Advanced types Part 1
 // Interfaces for Director and Teacher
 interface DirectorInterface {
   workFromHome(): string;
@@ -58,7 +58,7 @@ console.log(createEmployee("$500")); // Director instance
 
 // --------------------------------------------------
 
-// Task #6
+// Task #6: Creating Functions Specific to Employees
 
 // Define isDirector type predicate
 function isDirector(employee: Director | Teacher): employee is Director {
@@ -77,3 +77,20 @@ function executeWork(employee: Director | Teacher): string {
 // Test cases
 console.log(executeWork(createEmployee(200))); // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+// --------------------------------------------------
+
+// Task #7: String literal types
+
+//  Define string literal type
+type Subjects = "Math" | "History";
+
+//  Define teachClass function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") return "Teaching Math";
+  return "Teaching History";
+}
+
+// Test cases
+console.log(teachClass("Math")); // Teaching Math
+console.log(teachClass("History")); // Teaching History
