ab7972388653c84a70b000b9e5c0706d01d7494b
Bless John <166983244+BlessmanJay@users.noreply.github.com>
2025-09-03 00:34:12 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -6,3 +6,6 @@ export function insertRow(row: RowElement): RowID;
 
 export function deleteRow(rowID: RowID): void;
 export function updateRow(rowID: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number, 
+declare function deleteRow(rowId: RowID): void, 
+declare function updateRow(rowId: RowID): number, 
